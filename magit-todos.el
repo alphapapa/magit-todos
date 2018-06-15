6c1af8fd79ce315ac6e17b1fe505b19ba8963a2e
tobjaw <tobjaw@gmail.com>
2018-06-16 01:53:39 +0200

    chore: typo in package-requires

--- magit-todos.el
+++ magit-todos.el
@@ -5,7 +5,7 @@
 ;; Author: Adam Porter <adam@alphapapa.net>
 ;; URL: http://github.com/alphapapa/magit-todos
 ;; Version: 0.1-pre
-;; Package-Requires: ((emacs "25.2") (a) (anaphora) (dash) (f) (hl-todo) (kv) (magit) (pcre2el)
+;; Package-Requires: ((emacs "25.2") (a) (anaphora) (dash) (f) (hl-todo) (kv) (magit) (pcre2el))
 ;; Keywords: magit, vc
 
 ;;; Commentary:
