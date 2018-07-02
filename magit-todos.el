8082670e118fa8fe3565de1ae89af7eefe9314c1
Adam Porter <adam@alphapapa.net>
2018-07-01 23:51:09 -0500

    Fix: Add hl-todo and magit version numbers to headers
    Fixes #22.  Thanks to @matthew-piziak.

--- magit-todos.el
+++ magit-todos.el
@@ -5,7 +5,7 @@
 ;; Author: Adam Porter <adam@alphapapa.net>
 ;; URL: http://github.com/alphapapa/magit-todos
 ;; Version: 0.1-pre
-;; Package-Requires: ((emacs "25.2") (a "0.1.0") (anaphora "1.0.0") (async "1.9.2") (dash "2.13.0") (f "0.17.2") (hl-todo) (magit) (pcre2el "1.8") (s "1.12.0"))
+;; Package-Requires: ((emacs "25.2") (a "0.1.0") (anaphora "1.0.0") (async "1.9.2") (dash "2.13.0") (f "0.17.2") (hl-todo "1.9.0") (magit "2.13.0") (pcre2el "1.8") (s "1.12.0"))
 ;; Keywords: magit, vc
 
 ;;; Commentary:
