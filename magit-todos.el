d12e2e3ccad4b87d5df5285ade0c56ec5f46ad63
Adam Porter <adam@alphapapa.net>
2018-07-17 01:39:13 -0500

    Release 1.0.3

--- magit-todos.el
+++ magit-todos.el
@@ -4,7 +4,7 @@
 
 ;; Author: Adam Porter <adam@alphapapa.net>
 ;; URL: http://github.com/alphapapa/magit-todos
-;; Version: 1.0.3-pre
+;; Version: 1.0.3
 ;; Package-Requires: ((emacs "25.2") (a "0.1.0") (anaphora "1.0.0") (async "1.9.2") (dash "2.13.0") (f "0.17.2") (hl-todo "1.9.0") (magit "2.13.0") (pcre2el "1.8") (s "1.12.0"))
 ;; Keywords: magit, vc
 
